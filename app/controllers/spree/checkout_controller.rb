657921f84e926bce4e93ea8b1aaa6e164590e578
Lutvi Rosyady <moorails@gmail.com>
2019-03-22 01:03:48 +0700

    Fixing Midtrans Redirection

--- app/controllers/spree/checkout_controller.rb
+++ app/controllers/spree/checkout_controller.rb
@@ -1,8 +1,10 @@
 module Spree
   class CheckoutController < Spree::StoreController
-  	def update
-  	  redirect_to midtrans_charge_url(payment_method_id: Spree::Gateway::MidtransGateway.first.try(:id)) if Spree::Gateway::MidtransGateway.first.present? && @order.state == "payment"
-      super
+    before_action :midtrans_redirection, only: [:update]
+    
+    private
+  	def midtrans_redirection
+  	  redirect_to midtrans_charge_url(payment_method_id: Spree::Gateway::MidtransGateway.first.try(:id)) if Spree::Gateway::MidtransGateway.first.present? && params[:state] == "payment"
     end  	
   end
 end
