1e7ee293ec68560b0b5fa0eed6a7e6f67176a563
Lutvi Rosyady <moorails@gmail.com>
2019-03-22 00:47:57 +0700

    Adding Checkout Redirect To Midtrans Before Payment

---
+++ app/controllers/spree/checkout_controller.rb
@@ -0,0 +1,8 @@
+module Spree
+  class CheckoutController < Spree::StoreController
+  	def update
+  	  redirect_to midtrans_charge_url(payment_method_id: Spree::Gateway::MidtransGateway.first.try(:id)) if Spree::Gateway::MidtransGateway.first.present?
+  	  super
+    end  	
+  end
+end
